e1ebd0627ef27a1f54a6055525b7986433a7c25a
Kevin <shift8web@3057983.no-reply.drupal.org>
2017-07-28 08:22:26 -0400

    Cleaned up classes for desktop menu li items

--- README.md
+++ README.md
@@ -4,7 +4,7 @@
 * Tags: full screen nav,full screen navigation,sticky navigation,responsive nav menu,responsive menu,full screen menu,mobile menu
 * Requires at least: 3.0.1
 * Tested up to: 4.8
-* Stable tag: 1.31
+* Stable tag: 1.32
 * License: GPLv3
 * License URI: http://www.gnu.org/licenses/gpl-3.0.html
 
@@ -175,3 +175,6 @@ p.
 
 ### 1.31
 * Fixed CSS typo
+
+### 1.32
+* Cleaned up nav li classes (too many extra classes were generated)
--- components/functions.php
+++ components/functions.php
@@ -66,6 +66,27 @@ class Shift8_Walker_Nav_Menu_Desktop extends Walker_Nav_Menu {
 			$output .= "<ul class=\"fn-secondary-nav\">\n";
 		}
     }
+
+    function start_el(&$output, $item, $depth, $args) {
+        global $wp_query;
+        $this->curItem = $item;
+        $class_names = 'fn-dropdown fn-menu-item-' . $item->ID;
+
+        $output .= '<li class="' . $class_names . '">';
+        $attributes  = !empty($item->attr_title) ? ' title="' . esc_attr($item->attr_title) . '"' : '';
+        $attributes .= !empty($item->target) ? ' target="' . esc_attr($item->target) . '"' : '';
+        $attributes .= !empty($item->xfn) ? ' rel="' . esc_attr($item->xfn) . '"' : '';
+        $attributes .= !empty($item->url) ? ' href="' . esc_attr($item->url) . '"' : '';
+
+
+        $item_output = $args->before;
+        $item_output .= '<a' . $attributes . '>';
+        $item_output .= $args->link_before . apply_filters('the_title', $item->title, $item->ID);
+        $item_output .= '</a>';
+        $item_output .= $args->after;
+
+        $output .= apply_filters('walker_nav_menu_start_el', $item_output, $item, $depth, $args);
+	}
 }
 
 // Custom walker for mobile menu
